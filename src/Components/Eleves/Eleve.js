ab5a327331461c981c6db22ee742af9ca2b1b83a
alex dess <alexandre.dessoly1@gmail.com>
2023-06-26 16:21:19 +0200

    hook

--- src/Components/Eleves/Eleve.js
+++ src/Components/Eleves/Eleve.js
@@ -2,10 +2,37 @@ import React, {Component} from 'react';
 import './Eleve.css'
 
 class Eleve extends Component{
+
+    constructor(props){
+        super(props);
+        console.log('[Eleve.js] Constructor');
+    }
+
+    componentDidMount(){
+        console.log('[Eleve.js] ComponentDidMount');
+    }
+
+    componentWillUnmount(){
+        console.log('[Eleve.js] ComponentWillUnmount');
+    }
+
+    componentDidUpdate(){
+        console.log('[Eleve.js] ComponentDidUpdate');
+      }
+
+    shouldComponentUpdate(nextProps){
+        console.log('[Eleve.js] shouldComponentUpdate');
+        //si la prochaine valeur de nom est différente de la valeur du nom actuelle
+        if(nextProps.nom != this.props.nom){
+            return true;
+        }
+        return false;
+    }
+
     render(){
         return(
             <div className="eleve">
-                <h1>{this.props.nom}</h1>
+                <h1 onClick={this.props.clic}>{this.props.nom}</h1>
                 <p>Moyenne scolaire : <b>{this.props.moyenne}/20</b></p>
                 <p>Age :{Math.floor(Math.random()*100)} </p>
                 <i>{this.props.citation}</i>
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,31 +1,29 @@
 import './App.css';
 import { Component } from 'react';
+import React,{ useState } from 'react';
 //import eleve.js
 import Eleve from '../Components/Eleves/Eleve';
 
+function App(){
 
-class App extends Component{
-  state={
-    eleves:[
-      {
-        nom:'Eva Dupont',
-        moyenne:15,
-        citation:"Aller toujours plus loin"},
-      {
-        nom:'Elon Musk',
-        moyenne:20,
-        citation:"le feu ça brule et l'eau ça mouille"
-      }
-    ]
-  }
+  const [eleves,setEleves]=useState(
+    [{
+      nom:'Eva Dupont',
+      moyenne:15,
+      citation:"Aller toujours plus loin"
+    },
+    {
+      nom:'Elon Musk',
+      moyenne:20,
+      citation:"le feu ça brule et l'eau ça mouille"
+    }]
+  );
 
- 
-
-  buttonClickedHandler=()=>{
+  const buttonClickedHandler= nouveauNom =>{
     //on reprend le state d'avant et on le met dans la const nouveauState
     const nouveauState = [...this.state.eleves];
     //on dit ici que le prénom de la première personne sera "Steve Jobs"
-    nouveauState[0].nom="Steve Jobs"
+    nouveauState[0].nom=nouveauNom
     //on envoie ça dans le nouveau state
     this.setState({
       ...this.state,
@@ -33,29 +31,31 @@ class App extends Component{
     })
   }
 
-  render(){
     return(
       <div className="App">
         <h1>bienvenue dans la classe Terre</h1>
 
         <div>
-          <button onClick={this.buttonClickedHandler}>Mon bouton</button>
+           <button onClick={buttonClickedHandler.bind(this,"Elon Musk")}>Transformer le premier élève</button>
         </div>
 
         <Eleve
-          nom={this.state.eleves[0].nom}
-          moyenne={this.state.eleves[0].moyenne}
-          citation={this.state.eleves[0].citation}>
+          nom={eleves[0].nom}
+          moyenne={eleves[0].moyenne}
+          citation={eleves[0].citation}
+          clic={()=>this.buttonClickedHandler('Steve Jobs')}>
           
-          </Eleve>
+        </Eleve>
         <Eleve
-          nom={this.state.eleves[1].nom}
-          moyenne={this.state.eleves[1].moyenne}
-          citation={this.state.eleves[1].citation}>
+          nom={eleves[1].nom}
+          moyenne={eleves[1].moyenne}
+          citation={eleves[1].citation}
+          clic={()=>this.buttonClickedHandlerBis('Julie Martin')}>
           </Eleve>
       </div>
-    )
-  }
+  )
 }
+
+
 
 export default App;
